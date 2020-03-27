cbe9b656cf27ef558fc683c2a1eb7b681583fc55
TOM <matetothuk@gamil.com>
2020-03-27 21:06:02 +0100

    greeting setup

---
+++ src/main/java/hu/formulative/demo/controller/GreetingsController.java
@@ -0,0 +1,18 @@
+package hu.formulative.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping
+    public ResponseEntity getOrcFormData() {
+
+        return new ResponseEntity<>("Hello world", HttpStatus.OK);
+    }
+
+}
+
