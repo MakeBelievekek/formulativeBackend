91e6f83c2feda4922ef7f0fef694065453287844
TOM <matetothuk@gamil.com>
2020-03-28 16:22:31 +0100

    benne van a frontend

--- src/main/java/hu/formulative/demo/config/SpringWebConfig.java
+++ src/main/java/hu/formulative/demo/config/SpringWebConfig.java
@@ -21,7 +21,7 @@ public class SpringWebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://makebelievekek.github.io/angular-frontend/")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
