9f54abd0cbb5f59fe4b13e4ad3477e699479c312
TOM <matetothuk@gamil.com>
2020-03-28 17:52:41 +0100

    még töb dolog a CORS ban

--- src/main/java/hu/formulative/demo/config/SpringWebConfig.java
+++ src/main/java/hu/formulative/demo/config/SpringWebConfig.java
@@ -21,7 +21,7 @@ public class SpringWebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://makebelievekek.github.io/PRZandTOM/")
+                .allowedOrigins("http://localhost:4200", "https://makebelievekek.github.io/PRZandTOM/", "https://makebelievekek.github.io", "https://makebelievekek.github.io/:1")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
